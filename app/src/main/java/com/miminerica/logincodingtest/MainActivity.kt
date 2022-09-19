53dbe01b6cb7e77dd0000d808d9d09957441828b
mimi <kuni2097@gmail.com>
2022-09-19 17:03:58 +0900

    버튼 누르면 로그인 로직 구현

--- app/src/main/java/com/miminerica/logincodingtest/MainActivity.kt
+++ app/src/main/java/com/miminerica/logincodingtest/MainActivity.kt
@@ -2,10 +2,25 @@ package com.miminerica.logincodingtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginBtn.setOnClickListener {
+            val getId = idEdt.text.toString()
+            val getPw = pwEdt.text.toString()
+
+            if (getPw == "admin@test.com" && getId == "qwer") {
+                Toast.makeText(this, "관리자입니다", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
